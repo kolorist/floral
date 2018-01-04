e10bdb3b8b93849dfedf74559ec8ec9aa34782ea
home.nhat.nguyenhong <hongnhatbkhn@gmail.com>
2018-01-05 00:38:42 +0700

    + add custom build system

---
+++ make.bat
@@ -0,0 +1,42 @@
+@echo off
+
+pushd %~d0
+
+set msvcdir="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\"
+if not defined DevEnvDir call %msvcdir%vcvars32.bat >nul
+
+popd %~d0
+
+if not exist "build" md "build"
+if not exist "build/obj" md "build/obj"
+
+rem --------------------------------------------
+rem Compiling
+rem --------------------------------------------
+rem /Fo		object file output path
+rem /Fd		target file output path
+rem /Zi		generate .pdb
+rem /Od		disable optimization (Debug)
+rem --------------------------------------------
+echo Compiling floral...
+call cl.exe										^
+	/nologo										^
+	/I "."										^
+	/I "./include"								^
+	-D_CRT_SECURE_NO_WARNINGS					^
+	-DPLATFORM_WINDOWS							^
+	/Fo"./build/obj/"							^
+	/Fd"./build/"								^
+	/Zi											^
+	-Od											^
+	-c											^
+	src/cu/unitybuild_0.cpp
+
+rem --------------------------------------------
+rem Linking
+rem --------------------------------------------
+echo Linking floral...
+call lib.exe									^
+	/nologo										^
+	build/obj/unitybuild_0.obj					^
+	/OUT:"./build/floral.lib"
