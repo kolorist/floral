f8ade32eea48f5abffb979b1ca9c02e34988acc7
nhat.nguyenhong <hongnhatbkhn@gmail.com>
2018-03-27 17:15:16 +0700

    + code for file_stream

--- include/io/nativeio.h
+++ include/io/nativeio.h
@@ -27,18 +27,23 @@ namespace floral {
 			~file_stream();
 
 			u8									read_byte();
+			void								unread_byte();
 			c8									read_char();
+
+			const bool							is_eos();
 
 		public:
 			file_info							info;
-			u32									rpos;
+			u64									rpos;
 			u8*									buffer;
 	};
 
+	// -----------------------------------------
+
 	file_info									open_file(const_cstr filePath);
-	void										read_all_file(file_info& fileInfo, voidptr buffer);
-	void										read_all_file(file_info& fileInfo, file_stream& fileStream);
-	void										mmap_all_file(file_info& fileInfo, voidptr buffer);
+	void										read_all_file(const file_info& fileInfo, voidptr buffer);
+	void										read_all_file(const file_info& fileInfo, file_stream& fileStream);
+	void										mmap_all_file(const file_info& fileInfo, voidptr buffer);
 	void										close_file(file_info& fileInfo);
 }
 
--- src/io/nativeio.cpp
+++ src/io/nativeio.cpp
@@ -2,7 +2,7 @@
 
 namespace floral {
 
-	file_stream::File_stream()
+	file_stream::file_stream()
 		: rpos(0)
 		, buffer(nullptr)
 	{
@@ -19,12 +19,24 @@ namespace floral {
 		return retVal;
 	}
 
+	void file_stream::unread_byte()
+	{
+		rpos--;
+	}
+
 	c8 file_stream::read_char()
 	{
 		c8 retVal = buffer[rpos];
 		rpos++;
 		return retVal;
 	}
+
+	const bool file_stream::is_eos()
+	{
+		return (rpos >= info.file_size + 1);
+	}
+
+	// -----------------------------------------
 
 	file_info open_file(const_cstr filePath)
 	{
@@ -39,21 +51,21 @@ namespace floral {
 		return newFile;
 	}
 
-	void read_all_file(file_info& fileInfo, voidptr buffer)
+	void read_all_file(const file_info& fileInfo, voidptr buffer)
 	{
 		DWORD bytesRead = 0;
 		ReadFile(fileInfo.file_handle, buffer, fileInfo.file_size, &bytesRead, 0);
 	}
 
-	void read_all_file(file_info& fileInfo, file_stream& fileStream)
+	void read_all_file(const file_info& fileInfo, file_stream& fileStream)
 	{
 		DWORD bytesRead = 0;
 		fileStream.info = fileInfo;
 		fileStream.rpos = 0;
-		ReadFile(fileInfo.file_handle, fileStream.buffer, fileInfo.file_size, &byteRead, 0);
+		ReadFile(fileInfo.file_handle, fileStream.buffer, fileInfo.file_size, &bytesRead, 0);
 	}
 
-	void mmap_all_file(file_info& fileInfo, voidptr buffer)
+	void mmap_all_file(const file_info& fileInfo, voidptr buffer)
 	{
 	}
 
