e37d0ea1594cd048c0c058e65c7f85cdc40d33e6
nhat.nguyenhong <hongnhatbkhn@gmail.com>
2019-10-02 09:33:40 +0700

    minor

--- include/floral/math/utils.h
+++ include/floral/math/utils.h
@@ -4,6 +4,7 @@
 
 namespace floral {
 
+constexpr f32 pi = 3.1415926525898f;
 #define PI										3.1415926525898f
 
 #define		TEST_BIT(target, bitmask)			(target & bitmask)
@@ -11,27 +12,27 @@ namespace floral {
 #define		SET_BIT(target, bitmask)			(target = target | bitmask)
 #define		CLEAR_BIT(target, bitmask)			(target = target & (~bitmask))
 
-	template <class DType>
-	DType min(DType lhs, DType rhs) {
-		return (lhs > rhs ? rhs : lhs);
-	}
+template <class DType>
+DType min(DType lhs, DType rhs) {
+	return (lhs > rhs ? rhs : lhs);
+}
 
-	template <class DType>
-	DType max(DType lhs, DType rhs) {
-		return (lhs > rhs ? lhs : rhs);
-	}
+template <class DType>
+DType max(DType lhs, DType rhs) {
+	return (lhs > rhs ? lhs : rhs);
+}
 
-	template <class DType>
-	DType clamp(DType d, DType minValue, DType maxValue) {
-		return min(maxValue, max(d, minValue));
-	}
+template <class DType>
+DType clamp(DType d, DType minValue, DType maxValue) {
+	return min(maxValue, max(d, minValue));
+}
 
-	f32 to_radians(f32 degree);
-	f32 to_degree(f32 radians);
-	// solve ray-tracing quadratic, only success when we have 2 real solutions
-	const bool solve_rtquadratic(f32 A, f32 B, f32 C, f32& t0, f32& t1);
-	
-	// compute crc32 naive way
-	const u32 compute_crc32_naive(const_cstr nullTerminatedStr);
-	
+f32 to_radians(f32 degree);
+f32 to_degree(f32 radians);
+// solve ray-tracing quadratic, only success when we have 2 real solutions
+const bool solve_rtquadratic(f32 A, f32 B, f32 C, f32& t0, f32& t1);
+
+// compute crc32 naive way
+const u32 compute_crc32_naive(const_cstr nullTerminatedStr);
+
 }
