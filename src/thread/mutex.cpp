b09af22f41ddeb0a780597b81d542aee60d3d1db
nhat.nguyenhong <hongnhatbkhn@gmail.com>
2019-10-14 09:55:45 +0700

    fix filename

---
+++ src/thread/mutex.cpp
@@ -0,0 +1,14 @@
+#include "floral/thread/mutex.h"
+
+namespace floral {
+	lock_guard::lock_guard(mutex_ref_t mtx)
+		: ref_mutex(mtx)
+	{
+		ref_mutex.lock();
+	}
+
+	lock_guard::~lock_guard()
+	{
+		ref_mutex.unlock();
+	}
+}
---
+++ src/thread/thread.cpp
@@ -0,0 +1,1 @@
+#include "floral/thread/thread.h"
